2e5832e02625481d1c63baa65368ee5683a6ddb4
ATOMLubover <13809006129@163.com>
2026-09-06 12:21:47 +0800

    fix(ci): allow Deno line-length check to read PATH

--- scripts/ci-line-length.sh
+++ scripts/ci-line-length.sh
@@ -21,5 +21,5 @@ fi
     exit 1
 }
 
-deno run --allow-read --allow-run=git scripts/check-added-line-length.mjs \
+deno run --allow-read --allow-run=git --allow-env scripts/check-added-line-length.mjs \
     "$base_sha" "$baseline_file"
